a8542919504a5f5da2266864d05d7940a1537c0e
JonasPalms <jonas.p.sorensen@gmail.com>
2022-11-20 21:47:13 +0100

    fixed admin panel error

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const { DateTime } = require('luxon')
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/style.css')
   eleventyConfig.addPassthroughCopy('./src/assets')
+  eleventyConfig.addPassthroughCopy('./src/admin')
 
   // TIME FORMATTING FILTER
   eleventyConfig.addFilter('postDate', (dateObj) => {
